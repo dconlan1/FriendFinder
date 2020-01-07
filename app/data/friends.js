85a285038db8e14e6e10a0a9492ee3df833b9eb3
Dave Conlan <daveconlan@me.com>
2020-01-06 17:15:27 -0800

    fixed image sizes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,16 @@
 module.exports = [
 {
-    name: "bilbo",
-    photo: "",
+    name: "Dutch Warmblood",
+    photo: "https://i.imgur.com/lAHpbYK.jpg",
     scores: [1,2,3,2,5,1,2,3,4,5],
 },
 {
-    name: "baggons",
-    photo: "",
+    name: "Belgian Warmblood",
+    photo: "https://i.imgur.com/PFs9K6B.png",
     scores: [1,2,3,4,4,5,4,3,2,1],
 },
 {
-    name: "randolf",
+    name: "Akhal - Teke",
     photo: "https://www.petworlds.net/wp-content/uploads/2018/04/akhal-teke-images-850x550.jpg",
     scores: [1,1,3,1,1,1,2,3,4,5],
 }
